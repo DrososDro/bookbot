c5b9fc312a67f9ce6c028acbd173379d4dc34fa6
Drosos <drosinakis.drosos1@gmail.com>
2025-02-23 23:43:13 +0200

    finish project

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-book_path = "books/frankenstein.txt"
-
-
-def count_words(text):
-    return len(text.split())
+import sys
+from stats import get_num_words
 
 
 def count_letters(text):
@@ -16,13 +13,15 @@ def count_letters(text):
     return letter_dict
 
 
-def report(words, letters_dict):
+def report(words, letters_dict, book_path):
     print(f"--- Begin report of {book_path}")
     print(f"{words} words found in the document")
     sorted_dict = sorted(list(letters_dict.keys()))
     for key in sorted_dict:
+        if key in [" ", ".", "#"]:
+            continue
 
-        print(f"The {key} character was found {letters_dict[key]} times")
+        print(f"The {key}: {letters_dict[key]} times")
 
 
 def open_file(file_path):
@@ -31,10 +30,15 @@ def open_file(file_path):
 
 
 def main():
+    book_path = "books/frankenstein.txt"
+    args = sys.argv
+    if len(args) > 1:
+        book_path = args[1]
+
     book = open_file(book_path)
-    word_count = count_words(book)
+    word_count = get_num_words(book)
     letter_count = count_letters(book)
-    report(word_count, letter_count)
+    report(word_count, letter_count, book_path)
 
 
 if __name__ == "__main__":
